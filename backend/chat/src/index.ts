6892f1d012d5719f4651db0e315c4250f121064b
Rohit Singh <rohitsin28@example.com>
2025-08-29 21:14:04 +0530

    add cors in chat

--- backend/chat/src/index.ts
+++ backend/chat/src/index.ts
@@ -2,11 +2,13 @@ import express from 'express'
 import dotenv from 'dotenv'
 import { expressConfig } from './config/express.js';
 import { dbConnect } from './config/db.js';
+import cors  from 'cors';
 
 dotenv.config();
 const PORT = process.env.PORT || 5002;
 
 const app = express();
+app.use(cors())
 expressConfig(app);
 
 
