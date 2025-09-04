b8ca36fd12b2ce73ec1e2eb927962416554fc73d
Rohit Singh <rohitsin28@example.com>
2025-09-04 14:05:19 +0530

    fix multer bug

--- backend/chat/src/millerwares/multer.ts
+++ backend/chat/src/millerwares/multer.ts
@@ -15,7 +15,7 @@ export const upload = multer({
     storage: Storage,
     limits: {fileSize: 5 * 1024 * 1024},
     fileFilter: (req, file, cb) => {
-        if(file.mimetype.startsWith('/image/'))
+        if(file.mimetype.startsWith('image/'))
             cb(null, true)
         else
             cb(new Error('Only image files are allowed!'))
